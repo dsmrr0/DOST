3aad3f1bf46e78c8b748590b77b651cfdb177c22
dsmrr0 <56530696+dsmrr0@users.noreply.github.com>
2020-11-02 19:29:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>Employee Records</title>
+    <title>DOST-PCHRD</title>
      
     <!-- Latest compiled and minified Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
@@ -21,7 +21,7 @@
     <div class="container">
   
         <div class="page-header">
-            <h1>Employee Records</h1>
+            <h1>DOST</h1>
         </div>
      
         <?php
